b3980ae3b1b53e2e784836ad300fdb45367947de
Norbert Melzer <timmelzer@gmail.com>
2016-03-17 01:47:01 +0100

    Stuff

---
+++ lib/nobbz_blog.ex
@@ -0,0 +1,2 @@
+defmodule NobbzBlog do
+end
---
+++ mix.exs
@@ -0,0 +1,39 @@
+defmodule NobbzBlog.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :nobbz_blog,
+     version: "0.0.1",
+     elixir: "~> 1.0",
+     aliases: aliases,
+     deps: deps]
+  end
+
+  def aliases do
+    [c: ["deps.get", "deps.compile", "compile"], #, "docs"],
+     r: ["clean", "c"],
+     rf: ["deps.clean --all", "r"]]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
+  def application do
+    [applications: [
+      :logger
+    ]]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type `mix help deps` for more examples and options
+  defp deps do
+    [{:monolith, github: "NobbZ/monolith", branch: "master"}]
+  end
+end
